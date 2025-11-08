b0a1221a7f45463cac9abb64f83e0b29931c2336
ysnbk <130864850+ysnbk@users.noreply.github.com>
2025-11-08 20:57:48 +0000

    task 2

---
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -0,0 +1,40 @@
+import { Text, View } from "react-native";
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#90caf9",
+  },
+  largeText: {
+    fontSize: 30,
+    color: "#f44336",
+    marginBottom: 5,
+    fontWeight: "700",
+    fontVariant: ["small-caps"],
+  },
+  mediumText: {
+    fontSize: 20,
+    color: "#9c27b0",
+    marginBottom: 10,
+    fontWeight: "500",
+    textAlign: "right",
+  },
+  smallText: {
+    fontSize: 15,
+    color: "#2196f3",
+    fontWeight: "400",
+    textAlign: "center",
+  },
+});
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text>Entry Screen - Awesome</Text>
+      <View>
+  <Text style={styles.largeText}>Typescript is great if you practice more</Text>
+  <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
+  <Text style={styles.smallText}>ALX is awesome</Text>
+</View>
+    </View>
+  );
+}
